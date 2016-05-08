d0de8da68fce490d77aded44abf9233793129035
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2016-05-09 00:35:05 +0200

    Updated example to use faultline algorithm

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Terrain
 
-![Simple example](/examples/simple.png?raw=true "Simple example")
+![Simple example](/examples/simple.png?raw=true "Simple example") ![Simple example](/examples/simple_map.png?raw=true "Simple example")
 
 [Simple example](/examples/simple.jl) using [Rayly.jl](https://github.com/jagot/Rayly.jl).
--- examples/simple.jl
+++ examples/simple.jl
@@ -17,13 +17,19 @@ cam = SimpleCamera(Point{3}(0.,-1,-3),
                    1.0, 1.0, 1.0,
                    400, 400)
 
-hf = Heightfield(1.,1.,0.05rand(10,10))
-tris = Terrain.triangle_list(hf)
+hf = Terrain.Heightfield(1.,1.,100,100)
+Terrain.faultline!(hf, 500, Terrain.sinstep(0.1,0.1))
+
+compress(a::AbstractArray, mi, ma) = (a - minimum(a))*(ma-mi)/(maximum(a)-minimum(a)) + mi
+
+hf2 = hf
+hf2.y = compress(hf2.y, -0.2, 0.2)
+tris = Terrain.triangle_list(hf2)
 
 acc = ListAccelerator()
 add_tris!(acc, tris...)
 
-@time img = render(cam, JitteredSampler(9)) do ray::Ray
+@time img = render(cam) do ray::Ray
     hit = Rayly.intersect(acc, ray)
     hit != nothing ? shade(hit) : background
 end
